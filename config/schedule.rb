f958356706790e5675ce339b1a173b570e0c87bc
Christophe JUNIER <christophejunier@gmail.com>
2020-12-06 09:07:07 +0100

    adding some information about whenever command

--- config/schedule.rb
+++ config/schedule.rb
@@ -11,3 +11,12 @@ end
 every :day do
   rake 'update:test_instances_with_no_examiners'
 end
+
+# Usefull commands
+
+# bundle exec whenever --clear-crontab  # Cleaning whenever cron jobs
+
+# bundle exec whenever --update-crontab --set environment='development' # Updating crontab jobs and setting environment
+# bundle exec crontab -l   # Listing job on crontab
+
+# grep CRON  /var/log/syslog  # Greping Crontab logs
