95cea1b4e2e4c5eecb5173c7e6430c55e895cec7
Christophe JUNIER <christophejunier@gmail.com>
2020-12-08 19:37:18 +0100

    rubocop linting

--- spec/tasks/update_spec.rb
+++ spec/tasks/update_spec.rb
@@ -4,28 +4,28 @@ require 'rails_helper'
 
 Rails.application.load_tasks
 
-describe "test_instances_status_hash" do
+describe 'test_instances_status_hash' do
   let(:test_instance) { create(:test_instance, :en) }
 
-  it "update test_instances status hash" do
+  it 'update test_instances status hash' do
     test_instance
 
     expect(TestInstance.first.status_hash).to eq(nil)
-    run_task(task_name: "test_instances_status_hash")
+    run_task(task_name: 'test_instances_status_hash')
     expect(TestInstance.first.status_hash).to_not eq(nil)
   end
 end
 
-describe "test_instances_with_no_examiners" do
+describe 'test_instances_with_no_examiners' do
   let(:test_instance) { create(:test_instance, :en) }
   let(:examiner)      { create(:user, :examiner, :examiner_validated, :examiner_en) }
 
-  it "try to assign examiners to test instances with no examiners" do
+  it 'try to assign examiners to test instances with no examiners' do
     test_instance
     @examiner = examiner
 
     expect(test_instance.users.count).to eq(0)
-    run_task(task_name: "test_instances_with_no_examiners")
+    run_task(task_name: 'test_instances_with_no_examiners')
     expect(test_instance.users.count).to eq(1)
     expect(test_instance.users.first).to eq(@examiner)
   end
